9a3dafb3a2ebd3d2051a20a39c43b69cb81c8f3d
Isabellalura <00001109356699sp@al.educacao.sp.gov.br>
2024-10-29 20:29:27 -0300

    projeto-final

---
+++ script.js
@@ -0,0 +1,12 @@
+const avanca = Document.querySelectorAll ('.btn-proximo');
+
+
+avanca.forEach (buttom => {
+    buttom.addEventListener('click' , function(){
+        const atual = document.querySelector('.ativo');
+        const proximoPasso = 'passo-' + this.getAtribute('data-proximo');
+
+        atual.classList.remove('ativo');
+        document.getElementById(proximoPasso).classList.add('ativo')
+    })
+})
